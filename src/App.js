67f728bc16e05a6be80e1b99c7575650d7f7e90e
David Giraldo <degiraldod@gmail.com>
2023-06-04 17:44:08 -0400

    states added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { TodoTitle } from "./TodoTitle";
 import { TodoFilter } from "./TodoFilter";
 import { TodoList } from "./TodoList";
@@ -5,21 +6,33 @@ import { TodoItem } from "./TodoItem";
 import { AddTodoButton } from "./AddTodoButton";
 
 const defaultTodos = [
-  {text: "Leer un libro", completed: true},
-  {text: "Comprar el mercado", completed: false},
-  {text: "Ordenar cuarto", completed: false},
-  {text: "Terminar un curso", completed: false},
-]
+  { text: "Leer un libro", completed: true },
+  { text: "Comprar el mercado", completed: false },
+  { text: "Ordenar cuarto", completed: false },
+  { text: "Terminar un curso", completed: false },
+  { text: "Escuchar una canción", completed: true },
+];
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [todos, setTodos] = useState(defaultTodos);
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  console.log(searchValue);
   return (
     <>
-      <TodoTitle completed={16} total={25} />
-      <TodoFilter />
+      <TodoTitle completed={completedTodos} total={totalTodos} />
+      <TodoFilter searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map((todo, index) => (
-          <TodoItem key={index} todo={todo}/>
+        {filteredTodos.map((todo, index) => (
+          <TodoItem key={index} index={index} todo={todo} todos={todos} setTodos={setTodos}/>
         ))}
       </TodoList>
 

9646ffb31d15f61d028fd73124e7ada5d335eacb
David Giraldo <degiraldod@gmail.com>
2024-06-23 19:40:36 -0500

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
+    base: '/',
     build: {
       outDir: 'build',
       target: browserslistToEsbuild([

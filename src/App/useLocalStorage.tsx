626cc0de4e25a130610a9b1da89e8cb6e9dcbdf0
David Giraldo <degiraldod@gmail.com>
2024-07-22 21:34:03 -0500

    refactor: migrate from useState to useReducer in useLocalStorage

--- src/App/useLocalStorage.tsx
+++ src/App/useLocalStorage.tsx
@@ -1,10 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
+
+type State<T> = {
+  item: T;
+  loading: boolean;
+  error: boolean;
+  synchronized: boolean;
+};
+
+type Action<T> = { type: ActionTypes; payload?: T };
+
+const initialState = <T,>(initialValue: T): State<T> => ({
+  item: initialValue,
+  loading: true,
+  error: false,
+  synchronized: false,
+});
+
+enum ActionTypes {
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+  SAVE = 'SAVE',
+  SYNCHRONIZE = 'SYNCHRONIZE',
+}
+
+const reducerObject = <T,>(state: State<T>, payload?: T) => ({
+  [ActionTypes.ERROR]: { ...state, error: true, loading: false },
+  [ActionTypes.SUCCESS]: {
+    ...state,
+    item: payload as T,
+    loading: false,
+    synchronized: !state.synchronized && true,
+  },
+  [ActionTypes.SAVE]: { ...state, item: payload as T },
+  [ActionTypes.SYNCHRONIZE]: { ...state, synchronized: false, loading: true },
+});
+
+const reducer = <T,>(state: State<T>, action: Action<T>): State<T> => {
+  return reducerObject(state, action.payload)[action.type] || state;
+};
 
 const useLocalStorage = <T,>(itemName: string, initialValue: T) => {
-  const [item, setItem] = useState<T>(initialValue);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<boolean>(false);
-  const [synchronized, setSynchronized] = useState<boolean>(false);
+  const [state, dispatch] = useReducer(
+    reducer<T>,
+    initialState<T>(initialValue),
+  );
+  const { item, loading, error, synchronized } = state;
+
+  const onError = () => dispatch({ type: ActionTypes.ERROR });
+  const onSuccess = (parsedItem: T) =>
+    dispatch({ type: ActionTypes.SUCCESS, payload: parsedItem });
+  const onSave = (newItem: T) =>
+    dispatch({ type: ActionTypes.SAVE, payload: newItem });
+  const onSynchronize = () => dispatch({ type: ActionTypes.SYNCHRONIZE });
 
   useEffect(() => {
     setTimeout(() => {
@@ -17,28 +64,18 @@ const useLocalStorage = <T,>(itemName: string, initialValue: T) => {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
         } else {
-          parsedItem = JSON.parse(localStorageItem);
+          parsedItem = JSON.parse(localStorageItem) as T;
         }
-        setItem(parsedItem);
-        setLoading(false);
-        if (!synchronized) {
-          setSynchronized(true);
-        }
+        onSuccess(parsedItem);
       } catch (error) {
-        setError(true);
-        setLoading(false);
+        onError();
       }
     }, 3000);
   }, [synchronized]);
 
   const saveItem = (newItem: T) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  };
-
-  const synchronizeItems = () => {
-    setSynchronized(false);
-    setLoading(true);
+    onSave(newItem);
   };
 
   return {
@@ -46,7 +83,7 @@ const useLocalStorage = <T,>(itemName: string, initialValue: T) => {
     saveItem,
     loading,
     error,
-    synchronizeItems,
+    synchronizeItems: onSynchronize,
   };
 };
 

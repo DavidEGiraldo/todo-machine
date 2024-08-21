b2f80bbfdb43b21efe876042b6ac0ba333b63ef0
David Giraldo <degiraldod@gmail.com>
2024-08-20 19:59:29 -0500

    feat: improve eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
     'airbnb-typescript',
     'prettier',
     'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended', // Mejora la accesibilidad
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -15,10 +19,29 @@ module.exports = {
     },
     project: './tsconfig.json',
   },
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'jsx-a11y/anchor-is-valid': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
   settings: {
     react: {
       version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      alias: {
+        map: [['@components', './src/components']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
   },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,19 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
-  printWidth: 80,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.ts, *.tsx', 
+      options: {
+        parser: 'typescript',
+      },
+    },
+  ],
 };
